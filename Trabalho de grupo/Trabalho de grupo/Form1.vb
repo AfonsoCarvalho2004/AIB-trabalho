4bbd15800957c5e640bc13b15a74530b2a2195f2
AfonsoCarvalho2004 <124035825+AfonsoCarvalho2004@users.noreply.github.com>
2023-01-31 09:02:02 +0000

    Add project files

---
+++ Trabalho de grupo/Trabalho de grupo/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub CmdSair_Click(sender As Object, e As EventArgs) Handles CmdSair.Click
+        End
+
+    End Sub
+End Class
---
+++ Trabalho de grupo/Trabalho de grupo/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As informações gerais sobre um assembly são controladas por
+' conjunto de atributos. Altere estes valores de atributo para modificar as informações
+' associada a um assembly.
+
+' Revise os valores dos atributos do assembly
+
+<Assembly: AssemblyTitle("Trabalho de grupo")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Trabalho de grupo")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+<Assembly: Guid("f45cb8e0-4023-4d9b-bc0f-de6cb3e463e4")>
+
+' As informações da versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Versão Principal
+'      Versão Secundária 
+'      Número da Versão
+'      Revisão
+'
+' É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+' usando o "*" como mostrado abaixo:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
